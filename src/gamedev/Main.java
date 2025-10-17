8fffaabc8c68865a771111ecf5ed5dfb9269d20f
Alvieryy <lanceedash@gmail.com>
2025-10-17 19:01:21 +0800

    first commit

---
+++ src/gamedev/Main.java
@@ -0,0 +1,24 @@
+package gamedev;
+import javax.swing.*;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame();
+		 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 window.setResizable(false);
+		 window.setTitle("2D Adventure Game");
+		 window.setLocationRelativeTo(null);
+		 
+		 GamePanel panel = new GamePanel();
+		 window.add(panel);	 
+		 window.pack();
+		 
+		 window.setVisible(true);
+		 
+		 panel.startGameThread();
+	}
+
+}
